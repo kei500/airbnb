7dc8293e7219aab5467dbc4301c9ac75c92313db
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2017-02-27 00:31:53 +0900

    return access_token

--- lib/airbnb/v1.rb
+++ lib/airbnb/v1.rb
@@ -7,7 +7,7 @@ module Airbnb
         params = { form: params }
         params[:form][:client_id] = client_id
         response = HTTP.post("#{Airbnb::V1::URL}/authorize", params)
-        JSON.parse(response.to_str)
+        JSON.parse(response.to_str)['access_token']
       end
 
       def current_user(access_token)
