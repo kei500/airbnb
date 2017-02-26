5dd72a0b03e1e70799ddcf96929317d88b68199a
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2017-02-26 23:08:19 +0900

    /v1/account/active

--- lib/airbnb/v1.rb
+++ lib/airbnb/v1.rb
@@ -1,4 +1,4 @@
-require 'json'
+require 'http'
 
 module Airbnb
   class V1 < Base
@@ -7,6 +7,11 @@ module Airbnb
     class << self
       def authorize(params)
         post("#{Airbnb::V1::URL}/authorize", form: params)
+      end
+
+      def current_user(access_token)
+        HTTP.headers('X-Airbnb-OAuth-Token' => access_token)
+          .get("#{Airbnb::V1::URL}/account/active", params: { client_id: client_id })
       end
     end
   end
