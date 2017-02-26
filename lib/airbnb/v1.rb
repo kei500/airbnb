ab95d94bd4b70e1d99931bf11d69080bafcc8b94
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2017-02-26 18:53:57 +0900

    extend Base

--- lib/airbnb/v1.rb
+++ lib/airbnb/v1.rb
@@ -1,13 +1,12 @@
-require 'http'
 require 'json'
 
 module Airbnb
-  class V1
+  class V1 < Base
     URL = 'https://api.airbnb.com/v1'
 
     class << self
       def authorize(params)
-        HTTP.post("#{URL}/authorize", form: params)
+        post("#{Airbnb::V1::URL}/authorize", form: params)
       end
     end
   end
